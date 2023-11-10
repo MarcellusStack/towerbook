12c34353e66d08d18e22ecc5d0f976665a7f024b
aPohl <M.Pohl@drk-rostock.de>
2023-11-10 14:35:43 +0100

    update
    - added reusable query for checking if tower belongs to organization

--- server/queries/get-tower-day-incident.ts
+++ server/queries/get-tower-day-incident.ts
@@ -2,6 +2,7 @@ import { prisma } from "@server/db";
 import { authFilterQuery } from "@server/lib/utils/query-clients";
 
 import { type Tower, Role, TowerDay } from "@prisma/client";
+import { towerBelongsToOrganization } from "../lib/utils/tower-belong-to-organization";
 
 export type TowerDayIncidentProps = Pick<
   TowerDay,
@@ -22,15 +23,7 @@ export const getTowerDayIncident = authFilterQuery(async (search, user) => {
 
   if (!towerday) throw new Error("Towerday does not exist");
 
-  const checkTowerBelongsToOrganization = await prisma.tower.findFirst({
-    where: { id: towerday.towerId, organizationId: user.organizationId },
-    select: {
-      id: true,
-    },
-  });
-
-  if (!checkTowerBelongsToOrganization)
-    throw new Error("Towerday does not belong to your organization");
+  await towerBelongsToOrganization(towerday.towerId, user.organizationId);
 
   return towerday;
 }) as unknown as (
