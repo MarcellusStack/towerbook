5c0e589e8fabd715553bbb98e753710f23dbbe57
MarcellusStack <MarcellusStack@gmail.com>
2023-12-13 11:57:40 +0100

    added first aid operation forms based on type and table of contents

--- app/(dashboard)/protocols/first-aid-operation/[id]/page.tsx
+++ app/(dashboard)/protocols/first-aid-operation/[id]/page.tsx
@@ -39,12 +39,45 @@ import { getSearchList } from "@server/queries/get-search-list";
 import { SearchListForm } from "@components/forms/search-list-form";
 import { getFirstAidOperation } from "@server/queries/get-first-aid-operation";
 import { FirstAidOperationBigForm } from "@/components/forms/first-aid-operation-big-form";
+import { TableOfContents } from "@components/table-of-contents";
+import { FirstAidOperationSmallForm } from "@components/forms/first-aid-operation-small-form";
 
 export const dynamic = "force-dynamic";
+
+const links = [
+  { label: "Einsatz Informationen", link: "#operation-infos" },
+  { label: "Insert your title here", link: "#insert-your-title-here" },
+  { label: "Alarmierung durch", link: "#alarm" },
+  { label: "Eingesetzte Kräfte", link: "#forces-deployed" },
+  { label: "Eingesetzte Rettungsmittel", link: "#rescue-equipment-used" },
+  { label: "weitere Einsatzkräfte", link: "#additional-emergency-services" },
+  { label: "Einsatzart", link: "#type-of-use" },
+  { label: "Erkrankungen", link: "#diseases" },
+  { label: "Notfallgeschehen", link: "#emergency-event" },
+  { label: "Verletzungen", link: "#diseases" },
+  { label: "Messwerte", link: "#measurements" },
+  { label: "Bewusstsein", link: "#conciousness" },
+  { label: "Atmung", link: "#breathing" },
+  { label: "Kreislauf", link: "#circuit" },
+  { label: "Schmerzen", link: "#pain" },
+  { label: "Maßnahmen", link: "#measures" },
+  { label: "Ergebnis", link: "#result" },
+  { label: "Übergabe an", link: "#handover-to" },
+  { label: "Ersthelfer / Zeuge", link: "#first-responder-witness" },
+];
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const operation = await getFirstAidOperation(id, ["admin"]);
 
-  return <FirstAidOperationBigForm operation={operation} />;
+  return (
+    <>
+      {operation.type === "big" ? (
+        <FirstAidOperationBigForm operation={operation} />
+      ) : (
+        <FirstAidOperationSmallForm operation={operation} />
+      )}
+      <TableOfContents links={links} />
+    </>
+  );
 }
