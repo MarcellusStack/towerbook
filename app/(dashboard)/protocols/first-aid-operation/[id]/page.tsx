044fb06d9363c200eaa558e05a87ae7c9eca43c5
MarcellusStack <MarcellusStack@gmail.com>
2023-12-15 09:38:31 +0100

    added conditional rendering for first aid operation type including links

--- app/(dashboard)/protocols/first-aid-operation/[id]/page.tsx
+++ app/(dashboard)/protocols/first-aid-operation/[id]/page.tsx
@@ -1,42 +1,3 @@
-import { useDisclosure } from "@mantine/hooks";
-import { AuthLayout } from "@/components/layouts/auth-layout";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import Link from "next/link";
-import {
-  Title,
-  Text,
-  Button,
-  AppShell,
-  Burger,
-  Group,
-  Skeleton,
-  Anchor,
-  Center,
-  Loader,
-  Tabs,
-  Badge,
-  SimpleGrid,
-} from "@mantine/core";
-import { prisma } from "@/server/db";
-import SignUpForm from "@/components/forms/sign-up-form";
-import { redirect } from "next/navigation";
-import { PrimaryAppHeading } from "@components/typography/primary-app-heading";
-import { QuickSearchAdd } from "@/components/quick-search-add";
-import { UsersTable } from "@/components/tables/user-table";
-import { getUsers } from "@server/queries/get-users";
-
-import { SecondaryAppHeading } from "@components/typography/secondary-app-heading";
-import { getUser } from "@server/queries/get-user";
-import type { Profile } from "@prisma/client";
-import { roles } from "@/constants/roles";
-import RoleBadge from "@/components/role-badge";
-import UserDashboard from "@components/user-dashboard";
-import { getUserOverview } from "@/server/queries/get-user-overview";
-import { TowerDayWeatherForm } from "@/components/forms/tower-day-weather-form";
-import { getTowerDayWeather } from "@/server/queries/get-tower-day-weather";
-import { getSearchList } from "@server/queries/get-search-list";
-import { SearchListForm } from "@components/forms/search-list-form";
 import { getFirstAidOperation } from "@server/queries/get-first-aid-operation";
 import { FirstAidOperationBigForm } from "@/components/forms/first-aid-operation-big-form";
 import { TableOfContents } from "@components/table-of-contents";
@@ -44,7 +5,7 @@ import { FirstAidOperationSmallForm } from "@components/forms/first-aid-operatio
 
 export const dynamic = "force-dynamic";
 
-const links = [
+const bigLinks = [
   { label: "Einsatz Informationen", link: "#operation-infos" },
   { label: "Insert your title here", link: "#insert-your-title-here" },
   { label: "Alarmierung durch", link: "#alarm" },
@@ -66,6 +27,14 @@ const links = [
   { label: "Ersthelfer / Zeuge", link: "#first-responder-witness" },
 ];
 
+const smallLinks = [
+  { label: "Patient", link: "#patient" },
+  { label: "Einsatz Informationen", link: "#operation-infos" },
+  { label: "Hergang des Unfalls", link: "#emergency-event" },
+  { label: "Messwerte", link: "#measurements" },
+  { label: "Maßnahmen", link: "#measures" },
+];
+
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const operation = await getFirstAidOperation(id, ["admin"]);
@@ -73,11 +42,16 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <>
       {operation.type === "big" ? (
-        <FirstAidOperationBigForm operation={operation} />
+        <>
+          <FirstAidOperationBigForm operation={operation} />
+          <TableOfContents links={bigLinks} />
+        </>
       ) : (
-        <FirstAidOperationSmallForm operation={operation} />
+        <>
+          <FirstAidOperationSmallForm operation={operation} />
+          <TableOfContents links={smallLinks} />
+        </>
       )}
-      <TableOfContents links={links} />
     </>
   );
 }
