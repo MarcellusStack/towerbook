d1c7fcd1b9a27d8228eb9ed1c50819f566828be8
MarcellusStack <MarcellusStack@gmail.com>
2023-12-15 09:38:05 +0100

    added incomplete and revision status

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,7 @@
 export const status = {
   open: { color: "gray", label: "offen" },
   ongoing: { color: "yellow", label: "in Bearbeitung" },
+  revision: { color: "orange", label: "in Revision" },
   completed: { color: "green", label: "abgeschlossen" },
+  incomplete: { color: "red", label: "unvollständig" },
 };
