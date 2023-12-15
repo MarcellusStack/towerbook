2c6d18d060721ecb7503c7d7d20ebf66ea944224
MarcellusStack <MarcellusStack@gmail.com>
2023-12-15 09:40:44 +0100

    add links to searchlist page

--- app/(dashboard)/protocols/search-list/[id]/page.tsx
+++ app/(dashboard)/protocols/search-list/[id]/page.tsx
@@ -37,12 +37,26 @@ import { TowerDayWeatherForm } from "@/components/forms/tower-day-weather-form";
 import { getTowerDayWeather } from "@/server/queries/get-tower-day-weather";
 import { getSearchList } from "@server/queries/get-search-list";
 import { SearchListForm } from "@components/forms/search-list-form";
+import { TableOfContents } from "@components/table-of-contents";
 
 export const dynamic = "force-dynamic";
+
+const links = [
+  { label: "Gesuchte Person", link: "#wanted-person" },
+  { label: "Meldende Person", link: "#reporting-person" },
+  { label: "Zuletzt gesehen", link: "#last-seen" },
+  { label: "Maßnahmen", link: "#measures" },
+  { label: "Ergebnis", link: "#result" },
+];
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const searchlist = await getSearchList(id, ["admin"]);
 
-  return <SearchListForm searchlist={searchlist} />;
+  return (
+    <>
+      <SearchListForm searchlist={searchlist} />
+      <TableOfContents links={links} />
+    </>
+  );
 }
