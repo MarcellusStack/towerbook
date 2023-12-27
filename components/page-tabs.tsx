cbc147810431566fc40d0e25c2b5c4308e5b2632
MarcellusStack <MarcellusStack@gmail.com>
2023-12-27 09:30:12 +0100

    added page tabs
    equivalent to secondary page tabs without id

---
+++ components/page-tabs.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Divider, Tabs } from "@mantine/core";
+import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+
+export type PageTabsProps = {
+  page: string;
+  links: {
+    value: string;
+    icon: React.ReactNode;
+    label: string;
+  }[];
+};
+
+export const PageTabs = ({ page, links }: PageTabsProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  return (
+    <Tabs
+      variant="pills"
+      value={pathname.split("/")[3] ?? "/"}
+      onChange={(value) => router.push(`/${page}/${value}`)}
+    >
+      <Tabs.List>
+        {links.map((link) => (
+          <Tabs.Tab key={link.value} value={link.value} leftSection={link.icon}>
+            {link.label}
+          </Tabs.Tab>
+        ))}
+      </Tabs.List>
+      <Divider mt="sm" />
+    </Tabs>
+  );
+};
