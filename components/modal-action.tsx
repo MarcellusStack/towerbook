1fab45ff3a1cb8637c88cc2747572f48056f97fe
MarcellusStack <MarcellusStack@gmail.com>
2023-12-27 09:29:46 +0100

    added modal action

---
+++ components/modal-action.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Button, Stack, Text } from "@mantine/core";
+import { modals } from "@mantine/modals";
+import React from "react";
+
+export type ModalAction = {
+  color: string;
+  icon: React.ReactNode;
+  label: string;
+  content: React.ReactNode;
+};
+
+export const ModalAction = ({ color, icon, label, content }: ModalAction) => {
+  return (
+    <Button
+      color={color}
+      leftSection={icon}
+      onClick={() => {
+        modals.open({
+          title: `${label}`,
+          children: (
+            <>
+              <Stack gap="md">
+                <Text size="sm">
+                  Sind sie sicher, dass Sie diesen {label} wollen? Diese Aktion
+                  ist unwiderruflich.
+                </Text>
+                {content}
+              </Stack>
+            </>
+          ),
+        });
+      }}
+    >
+      {label}
+    </Button>
+  );
+};
