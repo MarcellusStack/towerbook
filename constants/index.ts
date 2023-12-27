3a8efc7e8815b441b9b4911ad4edc63063417432
MarcellusStack <MarcellusStack@gmail.com>
2023-12-27 09:35:09 +0100

    add revision models for action filter

--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,22 @@ export const status = {
   completed: { color: "green", label: "abgeschlossen" },
   incomplete: { color: "red", label: "unvollständig" },
 };
+
+export const revisionModels = [
+  {
+    type: "firstaidoperation",
+    model: "firstAidOperation",
+    url: "/protocols/first-aid-operation/",
+  },
+  {
+    type: "groupregistration",
+    model: "groupRegistration",
+    url: "/protocols/group-registration/",
+  },
+  {
+    type: "towerday",
+    model: "towerDay",
+    url: "/tower-days/",
+  },
+  { type: "searchlist", model: "searchList", url: "/protocols/search-list/" },
+];
