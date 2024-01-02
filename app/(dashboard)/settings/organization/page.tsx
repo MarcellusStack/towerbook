0211bfff067c7c1f06e7da037c5e04300863ad95
MarcellusStack <MarcellusStack@gmail.com>
2024-01-02 08:59:59 +0100

    added organization settings form to page

--- app/(dashboard)/settings/organization/page.tsx
+++ app/(dashboard)/settings/organization/page.tsx
@@ -1,8 +1,4 @@
-import { useDisclosure } from "@mantine/hooks";
-import { AuthLayout } from "@/components/layouts/auth-layout";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import Link from "next/link";
+import { OrganizationSettingsForm } from "@/components/forms/organization-settings-form";
 import {
   Title,
   Text,
@@ -18,24 +14,13 @@ import {
   Badge,
   SimpleGrid,
 } from "@mantine/core";
-import { prisma } from "@/server/db";
-import SignUpForm from "@/components/forms/sign-up-form";
-import { redirect } from "next/navigation";
-import { PrimaryAppHeading } from "@components/typography/primary-app-heading";
-import { QuickSearchAdd } from "@/components/quick-search-add";
-import { UsersTable } from "@/components/tables/user-table";
-import { getUsers } from "@server/queries/get-users";
-import { CreateUserForm } from "@/components/forms/invite-user-form";
-import { SecondaryAppHeading } from "@components/typography/secondary-app-heading";
-import { getUser } from "@server/queries/get-user";
-import type { Profile } from "@prisma/client";
-import { roles } from "@/constants/roles";
-import RoleBadge from "@/components/role-badge";
-import UserDashboard from "@components/user-dashboard";
-import { getUserOverview } from "@/server/queries/get-user-overview";
+
+import { getOrganizationSettings } from "@server/queries/organization";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  return "Organisation";
+  const organization = await getOrganizationSettings();
+
+  return <OrganizationSettingsForm organization={organization} />;
 }
