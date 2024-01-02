ccbce208350ac0e9af898aa3f9ff934d4f3388b1
MarcellusStack <MarcellusStack@gmail.com>
2024-01-02 08:58:33 +0100

    added query to get organization settings

---
+++ server/queries/organization.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@server/db";
+import { authAdminQuery } from "@server/lib/utils/query-clients";
+
+import { type Organization } from "@prisma/client";
+
+export type OrganizationSettingsProps = Pick<
+  Organization,
+  "name" | "towerLocations" | "todo" | "beachSections" | "weather" | "material"
+>;
+
+export const getOrganizationSettings =
+  authAdminQuery<OrganizationSettingsProps>(async (user) => {
+    const organization = await prisma.organization.findFirst({
+      where: {
+        id: user.organizationId as string,
+      },
+      select: {
+        name: true,
+        towerLocations: true,
+        todo: true,
+        beachSections: true,
+        weather: true,
+        material: true,
+      },
+    });
+
+    if (!organization) {
+      throw new Error("Organization not found");
+    }
+
+    return organization;
+  });
