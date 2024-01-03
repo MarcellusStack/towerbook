b9b6aabe4a95b76c6c2fc63b403f87139f01edf3
MarcellusStack <MarcellusStack@gmail.com>
2024-01-03 13:47:44 +0100

    added automatic typing

--- server/queries/get-first-aid-operation.ts
+++ server/queries/get-first-aid-operation.ts
@@ -1,21 +1,6 @@
 import { prisma } from "@server/db";
 import { authFilterQuery } from "@server/lib/utils/query-clients";
-
-import {
-  type Tower,
-  type Role,
-  type FirstAidOperation,
-  type Profile,
-} from "@prisma/client";
 import { cache } from "react";
-
-export type FirstAidOperationProps = FirstAidOperation;
-
-export type ExtendFirstAidOperationWithRelationProps =
-  FirstAidOperationProps & {
-    tower: Pick<Tower, "name" | "number" | "location">;
-    guardLeader: Pick<Profile, "firstName" | "lastName">;
-  };
 
 export const getFirstAidOperation = cache(
   authFilterQuery(async (search, user) => {
@@ -26,13 +11,12 @@ export const getFirstAidOperation = cache(
       },
       include: {
         guardLeader: { select: { firstName: true, lastName: true, id: true } },
-        tower: {
-          select: { name: true, location: true, number: true },
-        },
+        tower: { select: { name: true, location: true, number: true } },
       },
     });
-  }) as unknown as (
-    search: string,
-    requiredRoles: Role[]
-  ) => Promise<ExtendFirstAidOperationWithRelationProps>
+  })
 );
+
+export type FirstAidOperationProps = NonNullable<
+  Awaited<ReturnType<typeof getFirstAidOperation>>
+>;
