bf80b23a3fe243dc21659abf643b2c2b3b7829b3
MarcellusStack <MarcellusStack@gmail.com>
2024-01-16 14:09:15 +0100

    add accomodation actions

---
+++ services/accomodation/actions.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { prisma } from "@server/db";
+import { adminAction } from "@server/lib/utils/action-clients";
+import { createAccomodationSchema } from "@schemas/index";
+import { revalidatePath } from "next/cache";
+
+export const createAccomodation = adminAction(
+  createAccomodationSchema,
+  async (
+    { number, name, street, zipCode, location, availableBeds },
+    { user }
+  ) => {
+    try {
+      await prisma.accomodation.create({
+        data: {
+          number: number,
+          name: name,
+          street: street,
+          zipCode: zipCode,
+          location: location,
+          availableBeds: availableBeds,
+          organization: {
+            connect: {
+              id: user.organizationId as string,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error("Fehler beim erstellen des Zimmer");
+    }
+
+    revalidatePath("/", "layout");
+
+    return {
+      message: `Das Zimmer wurde erstellt`,
+    };
+  }
+);
