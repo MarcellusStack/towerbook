71a3e97631779956065179a080ed7a4165c69f62
MarcellusStack <MarcellusStack@gmail.com>
2024-01-16 14:09:31 +0100

    refactor get towers

--- server/queries/get-towers.ts
+++ server/queries/get-towers.ts
@@ -4,37 +4,29 @@ import { authFilterQuery } from "@server/lib/utils/query-clients";
 import { unstable_cache } from "next/cache";
 import { type Tower } from "@prisma/client";
 
-
-
 export const getTowers = authFilterQuery(async (search, user) => {
-  const towers = await unstable_cache(
-    async (search) => {
-      if (user.role.includes("admin")) {
-        return await prisma.tower.findMany({
-          where: {
-            organizationId: user.organizationId,
-            number: search ?? undefined,
-          },
-        });
-      }
+  if (user.role.includes("admin")) {
+    return await prisma.tower.findMany({
+      where: {
+        organizationId: user.organizationId,
+        number: search ?? undefined,
+      },
+    });
+  }
 
-      return await prisma.tower.findMany({
-        where: {
-          organizationId: user.organizationId,
-          number: search ?? undefined,
-          members: {
-            some: {
-              id: user.profileId,
-            },
-          },
+  return await prisma.tower.findMany({
+    where: {
+      organizationId: user.organizationId,
+      number: search ?? undefined,
+      members: {
+        some: {
+          id: user.profileId,
         },
-      });
+      },
     },
-    [],
-    {
-      tags: ["towers"],
-      revalidate: 1,
-    }
-  )(search);
-  return towers as Tower[];
+  });
 });
+
+export type TowersProps = NonNullable<Awaited<ReturnType<typeof getTowers>>>;
+
+export type TowerProps = NonNullable<Awaited<ReturnType<typeof getTowers>>>[0];
