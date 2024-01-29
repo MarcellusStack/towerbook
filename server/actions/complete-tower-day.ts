fa267d59270b131cf745373a47c93209ac4c7be7
MarcellusStack <MarcellusStack@gmail.com>
2024-01-29 15:10:35 +0100

    add prisma transaction to compleTowerday and update tower if towerday completed

--- server/actions/complete-tower-day.ts
+++ server/actions/complete-tower-day.ts
@@ -9,64 +9,93 @@ export const completeTowerDay = adminAction(
     id: z.string().min(1, { message: "Id wird benötigt" }),
   }),
   async ({ id }) => {
-
     //add transaction
     try {
-      const towerday = await prisma.towerDay.findUnique({
-        where: { id: id },
-        select: {
-          id: true,
-          watchmanStatus: true,
-          todoStatus: true,
-          incidentStatus: true,
-          weatherStatus: true,
-          materialStatus: true,
-          dutyplanStatus: true,
+      await prisma.$transaction(
+        async (tx) => {
+          const towerday = await tx.towerDay.findUnique({
+            where: { id: id },
+            select: {
+              id: true,
+              towerId: true,
+              watchmanStatus: true,
+              todoStatus: true,
+              incidentStatus: true,
+              weatherStatus: true,
+              materialStatus: true,
+              dutyplanStatus: true,
+            },
+          });
+
+          if (!towerday) {
+            throw new Error("Couldnt find tower day");
+          }
+
+          await tx.revision.deleteMany({
+            where: {
+              modelId: towerday.id,
+            },
+          });
+
+          const allCompleted =
+            towerday.watchmanStatus === "completed" &&
+            towerday.todoStatus === "completed" &&
+            towerday.incidentStatus === "completed" &&
+            towerday.weatherStatus === "completed" &&
+            towerday.materialStatus === "completed" &&
+            towerday.dutyplanStatus === "completed";
+
+          const updatedStatuses = {
+            watchmanStatus:
+              towerday.watchmanStatus !== "completed"
+                ? "incomplete"
+                : "completed",
+            todoStatus:
+              towerday.todoStatus !== "completed" ? "incomplete" : "completed",
+            incidentStatus:
+              towerday.incidentStatus !== "completed"
+                ? "incomplete"
+                : "completed",
+            dutyplanStatus:
+              towerday.dutyplanStatus !== "completed"
+                ? "incomplete"
+                : "completed",
+            weatherStatus:
+              towerday.weatherStatus !== "completed"
+                ? "incomplete"
+                : "completed",
+            materialStatus:
+              towerday.materialStatus !== "completed"
+                ? "incomplete"
+                : "completed",
+          };
+
+          const updatedTowerday = await tx.towerDay.update({
+            where: { id: id, status: { notIn: ["open", "completed"] } },
+            data: {
+              ...updatedStatuses,
+              status: allCompleted ? "completed" : "ongoing",
+            },
+            select: {
+              id: true,
+              status: true,
+            },
+          });
+
+          if (updatedTowerday && updatedTowerday.status === "completed") {
+            await tx.tower.update({
+              where: { id: towerday.towerId },
+              data: {
+                status: "beach_closed",
+              },
+            });
+          }
         },
-      });
-
-      if (!towerday) {
-        throw new Error("Couldnt find tower day");
-      }
-
-      await prisma.revision.deleteMany({
-        where: {
-          modelId: towerday.id,
-        },
-      });
-
-      const allCompleted =
-        towerday.watchmanStatus === "completed" &&
-        towerday.todoStatus === "completed" &&
-        towerday.incidentStatus === "completed" &&
-        towerday.weatherStatus === "completed" &&
-        towerday.materialStatus === "completed" &&
-        towerday.dutyplanStatus === "completed";
-
-      const updatedStatuses = {
-        watchmanStatus:
-          towerday.watchmanStatus !== "completed" ? "incomplete" : "completed",
-        todoStatus:
-          towerday.todoStatus !== "completed" ? "incomplete" : "completed",
-        incidentStatus:
-          towerday.incidentStatus !== "completed" ? "incomplete" : "completed",
-        dutyplanStatus:
-          towerday.dutyplanStatus !== "completed" ? "incomplete" : "completed",
-        weatherStatus:
-          towerday.weatherStatus !== "completed" ? "incomplete" : "completed",
-        materialStatus:
-          towerday.materialStatus !== "completed" ? "incomplete" : "completed",
-      };
-
-      await prisma.towerDay.update({
-        where: { id: id, status: { notIn: ["open", "completed"] } },
-        data: {
-          ...updatedStatuses,
-          status: allCompleted ? "completed" : "ongoing",
-        },
-      });
-
-      //get tower id and update tower to inactive if all tower days formulars are completed
+        {
+          maxWait: 15000,
+          timeout: 15000,
+        }
+      );
 
       revalidatePath("/", "layout");
 
