755e43d7046389f6fbeabd356b5780293feeaf25
MarcellusStack <MarcellusStack@gmail.com>
2024-02-12 15:27:06 +0100

    added condition for saving first to enable complete

--- components/tower-day-form-action.tsx
+++ components/tower-day-form-action.tsx
@@ -4,6 +4,7 @@ import { Button, Group } from "@mantine/core";
 import { IconShieldLock } from "@tabler/icons-react";
 import React from "react";
 import { useParams } from "next/navigation";
+import { useDisclosure } from "@mantine/hooks";
 
 export const TowerDayFormAction = ({
   updateStatus,
@@ -16,13 +17,20 @@ export const TowerDayFormAction = ({
     action: completeTowerDayFormStatus,
     executeNotification: `Turm Tag wird aktualisiert`,
   });
+  const [opened, handlers] = useDisclosure(false);
   const { id } = useParams();
   return (
     <Group justify="flex-end" gap="sm">
-      <Button variant="outline" type="submit" loading={updateStatus}>
+      <Button
+        variant="outline"
+        type="submit"
+        onClick={handlers.open}
+        loading={updateStatus}
+      >
         Speichern
       </Button>
       <Button
+        disabled={!opened}
         leftSection={<IconShieldLock />}
         loading={complete.status === "executing"}
         onClick={() => {
