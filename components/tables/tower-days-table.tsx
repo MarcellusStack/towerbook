f48e2439fc1e97c8f41a3ad701d966c7ec8e48f9
MarcellusStack <MarcellusStack@gmail.com>
2024-03-21 14:17:03 +0100

    remove old table

--- components/tables/tower-days-table.tsx
+++ components/tables/tower-days-table.tsx
@@ -6,11 +6,14 @@ import Link from "next/link";
 import { convertDate, convertTime } from "@utils/index";
 import { DeleteModalAction } from "@components/delete-modal-action";
 import { deleteTowerDay } from "@/server/actions/delete-tower-day";
-import { status } from "@/constants";
+import { status as globalStatus, tableColumnProps } from "@/constants";
 import { type TowerDaysProps } from "@/server/queries/tower-days";
 import { useSearchParams } from "next/navigation";
 import { TableLoader } from "@components/loader/table-loader";
 import { useGetTowerdays } from "@/data/towerdays";
+import { MantineTable } from "@components/mantine-table";
+import { ViewActionIcon } from "../view-action-icon";
+import { DeleteActionIcon } from "../delete-action-icon";
 
 export const TowerDayTableRow = ({
   towerday,
@@ -87,24 +90,82 @@ export function TowerDaysTable() {
 
   if (isPending) return <TableLoader />;
   return (
-    <Table verticalSpacing="sm" striped withTableBorder>
-      <Table.Thead>
-        <Table.Tr>
-          <Table.Th>Datum</Table.Th>
-          <Table.Th>Zeit</Table.Th>
-          <Table.Th>Status</Table.Th>
-          <Table.Th>Turm</Table.Th>
-          <Table.Th>Wachleiter</Table.Th>
-          <Table.Th>Turmleiter</Table.Th>
-          <Table.Th />
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>
-        {towerdays &&
-          towerdays.map((day) => (
-            <TowerDayTableRow key={day.id} towerday={day} />
-          ))}
-      </Table.Tbody>
-    </Table>
+    <>
+      <MantineTable
+        records={towerdays || []}
+        columns={[
+          {
+            accessor: "createdAt",
+            title: "Datum",
+            render: ({ createdAt }) => <>{convertDate(new Date(createdAt))}</>,
+            ...tableColumnProps,
+          },
+          {
+            accessor: "startedAt",
+            title: "Zeit",
+            render: ({ startedAt }) => <>{convertTime(new Date(startedAt))}</>,
+            ...tableColumnProps,
+          },
+          {
+            accessor: "status",
+            title: "Status",
+            render: ({ status }) => (
+              <Badge color={globalStatus[status].color}>
+                {globalStatus[status].label}
+              </Badge>
+            ),
+            ...tableColumnProps,
+          },
+          {
+            accessor: "tower",
+            title: "Turm",
+            render: ({ tower }) => (
+              <>
+                Turm {tower.number} {tower.location}
+              </>
+            ),
+            ...tableColumnProps,
+          },
+          {
+            accessor: "guardLeader",
+            title: "Wachleiter",
+            render: ({ guardLeader }) => (
+              <>
+                {guardLeader.firstName} {guardLeader.lastName}
+              </>
+            ),
+            ...tableColumnProps,
+          },
+          {
+            accessor: "towerLeader",
+            title: "Turmleiter",
+            render: ({ towerLeader }) => (
+              <>
+                {towerLeader.firstName} {towerLeader.lastName}
+              </>
+            ),
+            ...tableColumnProps,
+          },
+          {
+            accessor: "actions",
+            title: "Aktionen",
+            width: "0%",
+            render: (towerdays) => (
+              <Group gap={0} justify="flex-end">
+                <ViewActionIcon href={`/tower-days/${towerdays.id}`} />
+
+                <DeleteActionIcon
+                  id={towerdays.id}
+                  action={deleteTowerDay}
+                  model="Turm"
+                />
+              </Group>
+            ),
+            ...tableColumnProps,
+          },
+        ]}
+        storeKey="towerdays-table"
+      />
+    </>
   );
 }
